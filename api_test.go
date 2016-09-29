6820b8592fc7a193bdee0d8e3bcfe09b0bc0d889
Shane Starcher <shanestarcher@gmail.com>
2016-09-29 17:23:11 -0400

    adding acceptance test

--- api_test.go
+++ api_test.go
@@ -1,6 +1,7 @@
 package okta
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
@@ -10,5 +11,16 @@ func TestAPIFailure(t *testing.T) {
 	_, err := client.Authenticate("username", "password")
 	if !strings.Contains(err.Error(), "E0000007") {
 		t.Error("Expected E0000007, got ", err.Error())
+	}
+}
+
+func TestAcceptance(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	client := NewClient(os.Getenv("OKTA_ORG"))
+	_, err := client.Authenticate(os.Getenv("OKTA_USERNAME"), os.Getenv("OKTA_PASSWORD"))
+	if err != nil {
+		t.Error("Expected nil, got ", err.Error())
 	}
 }
